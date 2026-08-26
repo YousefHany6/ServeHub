38022cf501c69b0c76fa1cb3c1cb5ccfe675f078
Yousef Hani <hanyy4870@gmail.com>
2026-08-26 18:31:31 +0300

    Start  Add Entities

---
+++ ServeHub/ServeHub.Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ServeHub.Domain.Entities.Common
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public DateTimeOffset CreatedAt { get;  private set; } = DateTimeOffset.UtcNow;
+
+
+    }
+}
