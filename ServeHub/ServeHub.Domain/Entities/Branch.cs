0c1b056453a6a30a710158089371f364dc758d4d
Yousef Hani <hanyy4870@gmail.com>
2026-09-04 20:49:12 +0300

    Add Folder Constants

--- ServeHub/ServeHub.Domain/Entities/Branch.cs
+++ ServeHub/ServeHub.Domain/Entities/Branch.cs
@@ -8,14 +8,39 @@ namespace ServeHub.Domain.Entities
 
     public class Branch:BaseEntity
     {
-        public string branchName { get; set; }
-        public string branchAddress { get; set; }
+        public string branchName { get; private set; }
+        public string? branchAddress { get; private set; }
 
-        public ICollection<BranchEmployee>? branchEmployees { get; set; } = new List<BranchEmployee>();
-        public ICollection<BranchCategory>? branchCategories { get; set; } = new List<BranchCategory>();
-        public ICollection<BranchProductVariant>? branchProductVariants { get; set; } = new List<BranchProductVariant>();
-        public ICollection<Order>? orders { get; set; } = new List<Order>();
-        public ICollection<Area>? areas { get; set; } = new List<Area>();
-        public ICollection<Printer>? printers { get; set; } = new List<Printer>();
+        private readonly List<BranchEmployee> _branchEmployees = new();
+        public IReadOnlyCollection<BranchEmployee> BranchEmployees => _branchEmployees.AsReadOnly();
+
+        private readonly List<BranchCategory> _branchCategories = new();
+        public IReadOnlyCollection<BranchCategory> BranchCategories => _branchCategories.AsReadOnly();
+
+        private readonly List<BranchProductVariant> _branchProductVariants = new();
+        public IReadOnlyCollection<BranchProductVariant> BranchProductVariants => _branchProductVariants.AsReadOnly();
+
+        private readonly List<Order> _orders = new();
+        public IReadOnlyCollection<Order> Orders => _orders.AsReadOnly();
+
+        private readonly List<Area> _areas = new();
+        public IReadOnlyCollection<Area> Areas => _areas.AsReadOnly();
+
+        private readonly List<Printer> _printers= new();
+        public IReadOnlyCollection<Printer> Printers => _printers.AsReadOnly();
+
+
+        // Add New Branch
+        public Branch(string branchName, string branchAddress)
+        {
+            if (string.IsNullOrWhiteSpace(branchName))
+            {
+                throw new ArgumentException("");
+            }
+            
+            this.branchName = branchName;
+            this.branchAddress = branchAddress;
+        }
+
     }
 }
